df906c75afa8a24d2bc39e9ae8a237a0100a9928
Camilo Figueroa <cigotete@gmail.com>
2023-11-15 12:53:17 -0500

    Added state data for login and logout reducers.

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -12,10 +12,20 @@ export const authSlice = createSlice({
     },
     reducers: {
         login: ( state, { payload } ) => {
-
+            state.status = 'authenticated';
+            state.uid = payload.uid;
+            state.email = payload.email;
+            state.displayName = payload.displayName;
+            state.photoURL = payload.photoURL;
+            state.errorMessage = null;
         },
         logout: ( state, { payload } ) => {
-
+            state.status = 'not-authenticated',
+            state.uid = null;
+            state.email = null;
+            state.displayName = null;
+            state.photoURL = null;
+            state.errorMessage = payload?.errorMessage;
         },
         checkingCredentials: (state) => {
             state.status = 'checking';
