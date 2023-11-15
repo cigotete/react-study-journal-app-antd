e9167b9db4766e4000a2b6db9069e3afbca9ad01
Camilo Figueroa <cigotete@gmail.com>
2023-11-15 13:00:22 -0500

    Added async Signing with Google auth function at Thunks file, to trigger Google Auth.

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,7 +1,18 @@
-import { checkingCredentials } from './';
+import { singInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, login, logout } from './';
 
 export const checkingAuthentication = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+  };
+}
+
+export const startGoogleSignIn = () => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+    const result = await singInWithGoogle();
+    //console.log('result', result);
+    if ( !result.ok ) return dispatch( logout( result.errorMessage ) );
+    dispatch( login( result ))
   };
 }
