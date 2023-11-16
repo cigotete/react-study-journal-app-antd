b63d3784565f8f3f7b01ed3505687f9012d91d86
Camilo Figueroa <cigotete@gmail.com>
2023-11-16 01:11:43 -0500

    Registering user with user/email mode within Firebase.

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,27 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Button, Form, Input, Space, Typography } from 'antd';
 import { AuthLayout } from '../layout/AuthLayout';
+import { startCreatingUserWithEmailPassword } from '../../store/auth'
 
-const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-  },
-};
+export const RegisterPage = () => {
 
-const onFinish = (values) => {
-  console.log(values);
-};
+  const dispatch = useDispatch();
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not a valid email!',
+    },
+  };
 
-export const RegisterPage = () => (
+  const onFinish = (values) => {
+    const displayName = values.displayName;
+    const email = values.email;
+    const password = values.password;
+    dispatch (startCreatingUserWithEmailPassword({ email, password, displayName }));
+  };
+
+  const initialValues = {
+    displayName: "John Doe",
+    email: "johndoe@example.com",
+    password: "123456"
+  };
+
+  return (
     <AuthLayout title="Register">
       <Form
         name="register"
         onFinish={ onFinish }
         validateMessages={ validateMessages }
+        initialValues={ initialValues }
       >
         <Form.Item
-          name="username"
+          name="displayName"
           label="Username"
           rules={[
             {
@@ -72,4 +87,4 @@ export const RegisterPage = () => (
       </Link>
       </Space>
     </AuthLayout>
-);
+)};
--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,8 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -24,5 +28,26 @@ export const singInWithGoogle = async() => {
             errorMessage,
         }
     }
+}
+
+
+export const registerUserWithEmailPassword = async({ email, password, displayName }) => {
+
+  try {
+
+      const resp = await createUserWithEmailAndPassword( FirebaseAuth, email, password );
+      console.log('resp', resp);
+      const { uid, photoURL } = resp.user;
+
+      return {
+          ok: true,
+          uid, photoURL, email, displayName
+      }
+
+  } catch (error) {
+      console.log(error);
+      return { ok: false, errorMessage: error.message }
+  }
+
 }
 
